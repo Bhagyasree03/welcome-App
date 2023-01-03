8bc868b7707c6be8d17afede8ba6430df957e814
root <sanyambhagyasree09@gmail.com>
2023-01-03 17:10:32 +0000

    welcome-App

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,26 @@
+import {Component} from 'react'
+import './index.css'
+
+class Welcome extends Component {
+  state = {subscribed: false}
+
+  changeState = () => {
+    this.setState(prevState => ({subscribed: !prevState.subscribed}))
+  }
+
+  render() {
+    const {subscribed} = this.state
+    const text = subscribed ? 'Subscribed' : 'Subscribe'
+    return (
+      <div className="container">
+        <h1 className="heading">Welcome</h1>
+        <p className="message">Thank you! Happy Learning</p>
+        <button type="button" onClick={this.changeState}>
+          {text}
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Welcome
